13e5f30c595f137c2c4eb8fd0d88a3b0de35b8a0
Khánh Hoàng <6380436+hckhanh@users.noreply.github.com>
2024-01-12 14:01:49 +0700

    Remove mobile viewer restriction in admin-spa-redirect.js
    This commit simplifies the routing in the admin single-page application by removing the specific mobile viewer redirection within the handler function. It also removes unused 'routes' variable and now only checks if the URI does not include a dot, making it less restrictive.

--- spa/functions/admin-spa-redirect.js
+++ spa/functions/admin-spa-redirect.js
@@ -1,24 +1,10 @@
-const routes = /^\/(dashboard|login|password)|\/$/
 
 function handler(event) {
   const request = event.request
   const uri = request.uri
 
-  // Prevent mobile requests
-  const isMobileViewer = request.headers['cloudfront-is-mobile-viewer']
-  if (isMobileViewer && isMobileViewer.value === 'true') {
-    return {
-      statusCode: 307,
-      statusDescription: 'Temporary Redirect',
-      headers: {
-        'content-type': { value: 'text/html' },
-        location: { value: 'https://www.saladin.vn' },
-      },
-    }
-  }
-
-  // Check if this is a admin route
-  if (routes.test(uri) || !uri.includes('.')) {
+  // Check if this is a route
+  if (!uri.includes('.')) {
     request.uri = '/index.html'
   }
 
